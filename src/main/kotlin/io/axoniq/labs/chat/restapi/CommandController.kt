c25162032024f247f13757c2a0f9d71058642d65
Stefan Andjelkovic <sa.stefan.andjelkovic@gmail.com>
2022-03-03 15:33:03 +0100

    Fix build issues

--- src/main/kotlin/io/axoniq/labs/chat/restapi/CommandController.kt
+++ src/main/kotlin/io/axoniq/labs/chat/restapi/CommandController.kt
@@ -8,47 +8,45 @@ import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import java.util.concurrent.Future
-import javax.validation.Valid
-import javax.validation.constraints.NotEmpty
 
 @RestController
 class CommandController(private val commandGateway: CommandGateway) {
     @PostMapping("/rooms")
-    fun createChatRoom(@RequestBody room: @Valid Room): Future<String?>? {
+    fun createChatRoom(@RequestBody room: Room): Future<String?>? {
         requireNotNull(room.name) { "name is mandatory for a chatroom" }
         return commandGateway.send(CreateRoomCommand(room.roomId ?: UUID.randomUUID().toString(), room.name))
     }
 
     @PostMapping("/rooms/{roomId}/participants")
-    fun joinChatRoom(@PathVariable roomId: String, @RequestBody participant: @Valid Participant): Future<Void> {
+    fun joinChatRoom(@PathVariable roomId: String, @RequestBody participant: Participant): Future<Void> {
         requireNotNull(participant.name) { "name is mandatory for a chatroom" }
         return commandGateway.send(JoinRoomCommand(roomId, participant.name))
     }
 
     @PostMapping("/rooms/{roomId}/messages")
-    fun postMessage(@PathVariable roomId: String, @RequestBody message: @Valid Message): Future<Void> {
+    fun postMessage(@PathVariable roomId: String, @RequestBody message: Message): Future<Void> {
         requireNotNull(message.name) { "'name' missing - please provide a participant name" }
         requireNotNull(message.message) { "'message' missing - please provide a message to post" }
         return commandGateway.send(PostMessageCommand(roomId, message.name, message.message))
     }
 
     @DeleteMapping("/rooms/{roomId}/participants")
-    fun leaveChatRoom(@PathVariable roomId: String, @RequestBody participant: @Valid Participant): Future<Void?>? {
+    fun leaveChatRoom(@PathVariable roomId: String, @RequestBody participant: Participant): Future<Void?>? {
         requireNotNull(participant.name) { "name is mandatory for a chatroom" }
         return commandGateway.send(LeaveRoomCommand(roomId, participant.name))
     }
 
     data class Message(
-        val name: @NotEmpty String? = null,
-        val message: @NotEmpty String? = null
+        val name: String? = null,
+        val message: String? = null
     )
 
     class Participant(
-        val name: @NotEmpty String? = null
+        val name: String? = null
     )
 
     class Room(
         val roomId: String? = null,
-        val name: @NotEmpty String? = null
+        val name: String? = null
     )
 }
